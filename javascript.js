57b6815c2daf8140a4d58131fd112479b8f85b92
lexifogel <89878565+lexifogel@users.noreply.github.com>
2021-10-28 17:39:20 -0400

    before i delete the old dropdown code
    it's commented out in index.html

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+// function showCurrent() {
+    
+//     // ????
+//     document.querySelector(".dropdown-content").innerHTML += "<br>Go Blue!\n";
+// }
+
+// function submitMessage() {
+
+//     <button onclick="alert('Message sent!')">Submit</button>
+// }
+
+// document.querySelector(".dropdown-content").innerHTML += "<br>Go Blue!\n";
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
